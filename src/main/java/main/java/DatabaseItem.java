d2fe356d41175cbf60d5b06e33a402a5c639f7e6
Brandon <sourbrandon@gmail.com>
2017-09-30 13:23:42 -0400

    value types working not complete

--- src/main/java/main/java/DatabaseItem.java
+++ src/main/java/main/java/DatabaseItem.java
@@ -26,62 +26,17 @@ public class DatabaseItem {
     static {
         TYPE_VALUES = new HashMap<String, Double[]>();
         TYPE_VALUES.put("bomber", new Double[]{2.0, 5.0, 0.0, 0.0});
-        TYPE_VALUES.put("tee", new Double[]{4.5, 2.5, 0.0, 0.0);
-        TYPE_VALUES.put("jean", 0.0);
-        TYPE_VALUES.put("jogger", 7.0);
-        TYPE_VALUES.put("cardigan", 0.0);
-        TYPE_VALUES.put("sweatshirt", 3.0);
-        TYPE_VALUES.put("blouse", 0.0);
-        TYPE_VALUES.put("dress", 0.0);
-        TYPE_VALUES.put("skirt", 0.0);
-        TYPE_VALUES.put("polo", 0.0);
-        TYPE_VALUES.put("shirtdress", 0.0);
-        TYPE_VALUES.put("boatneck", 1.0);
-    }
-    private static final HashMap<String, Double> LEISURE_TYPE;
-    static {
-        LEISURE_TYPE = new HashMap<String, Double>();
-        LEISURE_TYPE.put("tee", 2.5);
-        LEISURE_TYPE.put("jean", 8.0);
-        LEISURE_TYPE.put("jogger", 2.0);
-        LEISURE_TYPE.put("cardigan", 6.0);
-        LEISURE_TYPE.put("sweatshirt", 3.0);
-        LEISURE_TYPE.put("blouse", 2.0);
-        LEISURE_TYPE.put("dress", 7.0);
-        LEISURE_TYPE.put("skirt", 6.0);
-        LEISURE_TYPE.put("polo", 1.0);
-        LEISURE_TYPE.put("shirtdress", 6.0);
-        LEISURE_TYPE.put("boatneck", 7.0);
-    }
-    private static final HashMap<String, Double> BUSINESS_TYPE;
-    static {
-        BUSINESS_TYPE = new HashMap<String, Double>();
-        BUSINESS_TYPE.put("tee", 0.0);
-        BUSINESS_TYPE.put("jean", 1.0);
-        BUSINESS_TYPE.put("jogger", 0.0);
-        BUSINESS_TYPE.put("cardigan", 3.0);
-        BUSINESS_TYPE.put("sweatshirt", 0.0);
-        BUSINESS_TYPE.put("blouse", 6.0);
-        BUSINESS_TYPE.put("dress", 5.0);
-        BUSINESS_TYPE.put("skirt", 6.0);
-        BUSINESS_TYPE.put("polo", 7.0);
-        BUSINESS_TYPE.put("shirtdress", 0.0);
-        BUSINESS_TYPE.put("boatneck", 0.0);
-    }
-    private static final HashMap<String, Double> FANCY_TYPE;
-    static {
-        FANCY_TYPE = new HashMap<String, Double>();
-        FANCY_TYPE.put("tee", 0.0);
-        FANCY_TYPE.put("jean", 0.0);
-        FANCY_TYPE.put("jogger", 0.0);
-        FANCY_TYPE.put("cardigan", 0.0);
-        FANCY_TYPE.put("sweatshirt", 0.0);
-        FANCY_TYPE.put("blouse", 0.0);
-        FANCY_TYPE.put("dress", 2.0);
-        FANCY_TYPE.put("skirt", 0.0);
-        FANCY_TYPE.put("polo", 0.0);
-        FANCY_TYPE.put("shirtdress", 0.0);
-        FANCY_TYPE.put("boatneck", 0.0);
+        TYPE_VALUES.put("tee", new Double[]{4.5, 2.5, 0.0, 0.0});
+        TYPE_VALUES.put("jean", new Double[]{0.0, 8.0, 1.0, 0.0});
+        TYPE_VALUES.put("jogger", new Double[]{7.0, 2.0, 0.0, 0.0});
+        TYPE_VALUES.put("cardigan", new Double[]{0.0, 6.0, 3.0, 0.0});
+        TYPE_VALUES.put("sweatshirt", new Double[]{3.0, 3.0, 0.0, 0.0});
+        TYPE_VALUES.put("blouse", new Double[]{0.0, 2.0, 6.0, 0.0});
+        TYPE_VALUES.put("dress", new Double[]{0.0, 7.0, 5.0, 2.0});
+        TYPE_VALUES.put("skirt", new Double[]{0.0, 6.0, 6.0, 0.0});
+        TYPE_VALUES.put("polo", new Double[]{0.0, 1.0, 7.0, 0.0});
+        TYPE_VALUES.put("shirtdress", new Double[]{0.0, 6.0, 0.0, 0.0});
+        TYPE_VALUES.put("boatneck", new Double[]{1.0, 7.0, 0.0, 0.0});
     }
 
     // Document{{_id=59cf60d1f9a4758a54139dfa, styleId=mw00750, gender=men,
