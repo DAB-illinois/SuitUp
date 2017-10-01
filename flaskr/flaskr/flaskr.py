be72609fb986435e3a038719c0195fc1b32e9c9d
Brandon <sourbrandon@gmail.com>
2017-09-30 22:33:40 -0400

    flask working

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
+from .mongoUtility import docs, validUrls, getDocFromLink, getAllRelatedItems, retrieveSortedCosineSimilarity
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
@@ -13,6 +13,15 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.lower()
-    return render_template("layout.html", link = processed_text)
+	url = request.form['text']
+
+	if str(url) not in list(validUrls):
+		return render_template("layout.html", link = "error: invalid url: '"+url+"'   "+str(validUrls))
+
+	databaseItem = getDocFromLink(url, docs)
+	if databaseItem == None or databaseItem.general_type.lower() == "ignore":
+		return render_template("layout.html", link = "error: invalid databaseItem")
+
+	filtered = getAllRelatedItems(databaseItem, docs)
+	sortedCosine = retrieveSortedCosineSimilarity(databaseItem, filtered)
+	return render_template("layout.html", link = sortedCosine[0].link)
