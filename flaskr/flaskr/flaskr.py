736cd92371ec0c2cdad8075427b53c0c626bd78c
coconut750750 <sourbrandon@gmail.com>
2017-10-03 21:22:59 -0500

    used jinja to make html easier to read

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -33,22 +33,18 @@ def my_form_post():
 		need = ["top","bottom","footwear","outer"]
 		need.remove(haveType)
 	
-	have = []
-	haveCosine = []
+	have = {}
+	side = ["left", "right"]
 	
 	for recommendItem in sortedCosine:
 		general_type = recommendItem[0].general_type
 		if general_type in need:
-			have.append(recommendItem[0])
-			haveCosine.append(recommendItem[1])
+			have[recommendItem[0]] = [recommendItem[1], side[len(have) % len(side)]]
 			need.remove(recommendItem[0].general_type)
 
  # TODO: change render_template to accept only one item.
-	return render_template("layout.html", visibility="visible", 
-		name0=query_item.name, link0=query_item.link, img_link0=query_item.pic_link, vector0=round_to_fifth_dec_list(query_item.vector),
-		name1=have[0].name, link1=have[0].link, img_link1=have[0].pic_link, price1=have[0].price, description1=fix_description(have[0].description), cosine1=round_to_fifth_dec(haveCosine[0]), vector1=round_to_fifth_dec_list(have[0].vector),
-		name2=have[1].name, link2=have[1].link, img_link2=have[1].pic_link, price2=have[1].price, description2=fix_description(have[1].description), cosine2=round_to_fifth_dec(haveCosine[1]), vector2=round_to_fifth_dec_list(have[1].vector),
-		name3=have[2].name, link3=have[2].link, img_link3=have[2].pic_link, price3=have[2].price, description3=fix_description(have[2].description), cosine3=round_to_fifth_dec(haveCosine[2]), vector3=round_to_fifth_dec_list(have[2].vector))
+	return render_template("layout.html", visibility="visible", results=have,
+		name0=query_item.name, link0=query_item.link, img_link0=query_item.pic_link, vector0=round_to_fifth_dec_list(query_item.vector))
 
 def fix_description(description):
 	description = description.split("•")
